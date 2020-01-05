14dabbabd2fcfec5e5e3ac6d056a8919329c89ed
Ziru2k44 <bernardo.b.lourenco@gmail.com>
2020-01-05 17:30:48 +0000

    Added facebook album next page controller

--- src/main/java/com/primlook/memester/controller/FacebookController.java
+++ src/main/java/com/primlook/memester/controller/FacebookController.java
@@ -2,13 +2,9 @@ package com.primlook.memester.controller;
 
 import com.primlook.memester.infrastructure.facebook.domain.album.AlbumResponse;
 import com.primlook.memester.infrastructure.facebook.service.UploadService;
-import com.primlook.memester.security.CurrentUser;
-import com.primlook.memester.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Bernardo on 12/30/2019
@@ -24,5 +20,13 @@ public class FacebookController {
     @PreAuthorize("hasRole('USER')")
     public AlbumResponse getAlbums() {
         return uploadService.getAlbums();
+    }
+
+    @GetMapping("/albums/{id}")
+    @PreAuthorize("hasRole('USER')")
+    public AlbumResponse getAlbums(@PathVariable("id") String albumId,
+                                   @RequestParam(name = "after", required = false) String after,
+                                   @RequestParam(name = "before", required = false) String before) {
+        return uploadService.getPagingAlbums(albumId, after, before);
     }
 }
